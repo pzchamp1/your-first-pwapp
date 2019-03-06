bb15a4fc4459026b36daed1cedfe6fd73d1aed0d
Patrick <pzchamp1@gmail.com>
2019-03-06 19:43:03 +0200

    Step 03
    Dummy data for chicago to test program flow;

--- scripts/app.js
+++ scripts/app.js
@@ -309,6 +309,42 @@
       }
     }
   };
+
+  var initialWeatherForecastChicago = {
+    key: '2379574',
+    label: 'Chicago, IL',
+    created: '2016-07-22T01:00:00Z',
+    channel: {
+      astronomy: {
+        sunrise: "5:43 am",
+        sunset: "8:21 pm"
+      },
+      item: {
+        condition: {
+          text: "Windy",
+          date: "Thu, 21 Jul 2016 09:00 PM EDT",
+          temp: 88,
+          code: 24
+        },
+        forecast: [
+          {code: 44, high: 86, low: 70},
+          {code: 44, high: 94, low: 73},
+          {code: 4, high: 95, low: 78},
+          {code: 24, high: 75, low: 89},
+          {code: 24, high: 89, low: 77},
+          {code: 44, high: 92, low: 79},
+          {code: 44, high: 89, low: 77}
+        ]
+      },
+      atmosphere: {
+        humidity: 99
+      },
+      wind: {
+        speed: 66,
+        direction: 444
+      }
+    }
+  };
   // TODO uncomment line below to test app with fake data
       app.updateForecastCard(initialWeatherForecast);
 
