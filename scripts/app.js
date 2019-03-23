15f3df1f331aae2b003946cd8002651b6d0aa93e
Patrick <pzchamp1@gmail.com>
2019-03-23 16:00:46 +0200

    Weather Service Working

--- scripts/app.js
+++ scripts/app.js
@@ -166,18 +166,16 @@
     // TODO add cache logic here
 
     // Fetch the latest data.
-    var header = getOAuthHeader();
-    var request = new XMLHttpRequest();
-    request.setRequestHeader('Authorization', header.AuthHeader);
-    request.setRequestHeader('X-Yahoo-App-Id', header.AppId);
+    var header = getOAuthHeader(key);
+    var request = new XMLHttpRequest();    
     request.onreadystatechange = function() {
       if (request.readyState === XMLHttpRequest.DONE) {
         if (request.status === 200) {
           var response = JSON.parse(request.response);
-          var results = response.query.results;
-          results.key = key;
-          results.label = label;
-          results.created = response.query.current_observation.pubDate;
+          var results = response;//response.query.results;
+          results.location.woeid = key//results.key = key;
+          results.location.city = label;
+          results.current_observation.pubDate = response.current_observation.pubDate;
           app.updateForecastCard(results);
         }
       } else {
@@ -186,6 +184,8 @@
       }
     };
     request.open('GET', url);
+    request.setRequestHeader('Authorization', header.AuthHeader);
+    request.setRequestHeader('X-Yahoo-App-Id', header.AppId);
     request.send();
   };
 
